fbd85217db1bc9736479bc340ddbe83abf0dc9a1
ramanaditya <adityaraman96@gmail.com>
2020-02-07 04:56:14 +0530

    Freecodecamp

---
+++ JavaScript-algorithms-and-data-structures-certification/Basic-JavaScript/110-use-recursion-to-create-a-range-of-numbers.js
@@ -0,0 +1,10 @@
+function rangeOfNumbers(startNum, endNum) {
+  if (endNum < startNum) {
+    return [];
+  } else {
+    const rangeArray = rangeOfNumbers(startNum,endNum - 1);
+    rangeArray.push(endNum);
+    return rangeArray;
+  }
+};
+console.log(rangeOfNumbers(5,8))
---
+++ JavaScript-algorithms-and-data-structures-certification/Basic-JavaScript/45-shopping-list.js
@@ -0,0 +1,6 @@
+var myList = [];
+myList.push(["Chocolate Bar", 15]);
+myList.push(["Chocolate Bar", 15]);
+myList.push(["Chocolate Bar", 15]);
+myList.push(["Chocolate Bar", 15]);
+myList.push(["Chocolate Bar", 15]);
